8464a1db39b5357ed979e802aa02e20e830de829
Maxime Aubaret <maxime@plabs.co>
2014-03-19 08:49:51 +0100

    Debian install script: Fix insertion of fish repository

--- scripts/install/debian.sh
+++ scripts/install/debian.sh
@@ -11,7 +11,7 @@ sudo rake install
 
 # Install fish
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key D880C8E4
-sudo echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/2/Debian_6.0/ ./' > /etc/apt/sources.list.d/fish-shell.list
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/2/Debian_6.0/ ./' > /etc/apt/sources.list.d/fish-shell.list"
 sudo apt-get update
 sudo apt-get install -y fish
 
