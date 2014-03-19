8af73353ad7b2808c4411dbb9592cd2b817e8595
Maxime Aubaret <maxime@plabs.co>
2014-03-19 08:37:42 +0100

    Add installation of hub in debian/ubuntu

--- scripts/install/debian.sh
+++ scripts/install/debian.sh
@@ -1,4 +1,14 @@
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key D880C8E4
-echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/2/Debian_6.0/ ./' > /etc/apt/sources.list.d/fish-shell.list
-apt-get update
-apt-get install -y fish
+# Install hub
+sudo apt-get install -y rake
+mkdir -p ~/src/
+cd ~/src/
+git clone https://github.com/github/hub
+cd hub
+sudo rake install
+
+# Install fish
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key D880C8E4
+sudo echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/2/Debian_6.0/ ./' > /etc/apt/sources.list.d/fish-shell.list
+sudo apt-get update
+sudo apt-get install -y fish
+
--- scripts/install/ubuntu.sh
+++ scripts/install/ubuntu.sh
@@ -1,3 +1,12 @@
+# Install hub
+sudo apt-get install -y rake
+mkdir -p ~/src/
+cd ~/src/
+git clone https://github.com/github/hub
+cd hub
+sudo rake install --prefix=/usr/local
+
+# Install fish
 sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
 sudo apt-get install -y fish
