a4293bdefb79a1af9ef23bffe5822e0a7b6a5459
Maxime Aubaret <maxime@plabs.co>
2014-03-19 08:28:08 +0100

    Add Ubuntu/Debian install scripts

---
+++ scripts/install/debian.sh
@@ -0,0 +1,4 @@
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key D880C8E4
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/2/Debian_6.0/ ./' > /etc/apt/sources.list.d/fish-shell.list
+apt-get update
+apt-get install fish
