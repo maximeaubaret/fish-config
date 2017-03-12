6bf45a52a50ca6e1f1ec9657004369264d3bbfef
Maxime Aubaret <maxime@moveyourbuddy.io>
2017-03-12 03:44:43 +0100

    Remove thefuck

--- config.fish
+++ config.fish
@@ -11,9 +11,6 @@ alias git=hub
 set __fish_git_prompt_show_informative_status yes
 set __fish_git_prompt_showcolorhints yes
 
-# FUCK
-eval (thefuck --alias | tr '\n' ';')
-
 abbr -a rns react-native start
 abbr -a rnios react-native run-ios
 abbr -a rnandroid react-native run-android
